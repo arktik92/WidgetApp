b7a785e3296310935d7f68e1785672733367b802
Esteban SEMELLIER <esteban.semellier@mobiapps.fr>
2023-11-22 13:46:55 +0100

    Initial Commit

---
+++ WidgetApp/WidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAppApp.swift
+//  WidgetApp
+//
+//  Created by Esteban SEMELLIER on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
